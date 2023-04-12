d2018f8f4c5aec99cfa11cba40542d5e38b6b124
robel <wakgarirobel@gmail.com>
2023-04-12 13:11:18 +0300

    Add project files.

---
+++ CalculateDepth/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculateDepth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test sample
+            Branch br1 = new Branch();
+            Branch br2 = new Branch();
+            Branch br3 = new Branch();
+            Branch br4 = new Branch();
+            Branch br5 = new Branch();
+
+            br1.branches = new List<Branch> { br2 };
+            br2.branches = new List<Branch> { br3 };
+            br3.branches = new List<Branch> { br4 };
+            br4.branches = new List<Branch> { br5 };
+
+            int depth = calcDepth(br1);
+            Console.WriteLine($"The depth of the provided structure is {depth}.");
+        }
+
+        static int calcDepth(Branch branch)
+        {
+            if (branch.branches == null || branch.branches.Count == 0)
+            {
+                return 1;
+            }
+
+            int branchDepth = 0;
+            foreach (Branch b in branch.branches)
+            {
+                int depth = calcDepth(b);
+                if (depth > branchDepth)
+                {
+                    branchDepth = depth;
+                }
+            }
+
+            return branchDepth + 1;
+        }
+    }
+    
+}
---
+++ LesserAngleBetweenHoursAndMinutes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LesserAngleBetweenHoursAndMinutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter hours:");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter minutes:");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+
+            double hour_angle = (hours % 12 + (double)minutes / 60) * 30;
+            double minute_angle = minutes * 6;
+            double angle = Math.Abs(hour_angle - minute_angle);
+
+            if (angle > 180)
+            {
+                angle = 360 - angle;
+            }
+
+            Console.WriteLine($"Lesser angle in degrees between hours arrow and minutes arrow{angle}.");
+        }
+    }
+}
